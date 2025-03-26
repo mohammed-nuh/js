7388189ff8030093e0c737d8bfa150491d25e493
Nuh <mohammednuhabuhuseina@gmail.com>
2025-03-26 15:30:26 +0530

    basic js files

---
+++ calc.js
@@ -0,0 +1,13 @@
+let x = 10;
+let y = 5;
+
+let result = x + y;
+console.log(result);
+result = x - y;
+console.log(result);
+result = x * y;
+console.log(result);
+result = x / y;
+console.log(result);
+result = x % y;
+console.log(result);
