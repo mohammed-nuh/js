58379bdbee153fe8711a23831b374127b9c65444
Nuh <mohammednuhabuhuseina@gmail.com>
2025-03-27 15:35:49 +0530

    ifelseif and switch case

---
+++ ifelseif.js
@@ -0,0 +1,13 @@
+let marks = 65;
+
+if (marks >= 90) {
+    console.log("Your grade is A+");    
+} else if (marks >= 80) {
+    console.log("Your grade is A");    
+} else if (marks >= 70) {
+    console.log("Your grade is B+");    
+} else if (marks >= 60){
+    console.log("Your grade is B");    
+} else {
+    console.log("Your grade is C");    
+}
---
+++ switch_case.js
@@ -0,0 +1,19 @@
+let grade = "A";
+
+switch (grade) {
+  case "A+":
+    console.log("You scored distinction!");
+    break;
+  case "A":
+    console.log("You scored First Class!");
+    break;
+  case "B+":
+    console.log("You scored Second Class!");
+    break;
+  case "B+":
+    console.log("You scored Third Class!");
+    break;
+  default:
+    console.log("You failed!");
+    break;
+}
