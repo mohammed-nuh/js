b46cb0548a6926b0d024b614e4bb10b81aed50dc
mohammed-nuh <mohammednuhabuhuseina@gmail.com>
2025-04-09 19:23:52 +0530

    square fns

---
+++ square_arrowFn.js
@@ -0,0 +1,5 @@
+let square = n => n * n;
+
+let n = 6;
+console.log(`Square of ${n} is ${square(n)}`);
+
---
+++ square_fn.js
@@ -0,0 +1,6 @@
+function square(n) {
+    return n * n;
+}
+
+let n = 5;
+console.log(`Square of ${n} is ${square(n)}`);
