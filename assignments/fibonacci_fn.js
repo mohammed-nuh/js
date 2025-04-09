13be928fc56e3913372ec6a9007c5320c3308a02
Nuh <mohammednuhabuhuseina@gmail.com>
2025-04-09 15:43:25 +0530

    assignments

---
+++ assignments/fibonacci_fn.js
@@ -0,0 +1,13 @@
+function printFibonacciSeries(nth){
+    let fibonacciSeries = [];
+    for (let i = 0; i < nth; i++) {
+        if (i == 0 || i == 1) {
+            fibonacciSeries.push(1); 
+        } else {
+            fibonacciSeries.push(fibonacciSeries[i-2] + fibonacciSeries[i-1]); 
+        } 
+    }
+    console.log(`Fibonacci Series till ${nth} is ${fibonacciSeries}`); 
+}
+
+printFibonacciSeries(7);
---
+++ assignments/odd_even_arrowFn.js
@@ -0,0 +1,10 @@
+let checkOddEven = num => {
+    if (num % 2 == 0) {
+        console.log(`${num} is even`);
+    } else {
+        console.log(`${num} is odd`);
+    }
+}
+
+checkOddEven(7);
+checkOddEven(12);
