6059299626beeb6036a0a4c3924cc963e969b764
mohammed-nuh <mohammednuhabuhuseina@gmail.com>
2025-04-12 19:54:29 +0530

    map and filter functions

---
+++ filter.js
@@ -0,0 +1,5 @@
+let priceOfProducts = [1050, 500, 1400, 250];
+let filteredProducts = priceOfProducts.filter(product => product >= 1000);
+
+console.log(`priceOfProducts array => ${priceOfProducts}`);
+console.log(`filteredProducts array => ${filteredProducts}`);
