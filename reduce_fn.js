ffe904bb55410570e7383c27cf369a85fc9e2ba7
mohammed-nuh <mohammednuhabuhuseina@gmail.com>
2025-04-15 20:51:16 +0530

    ternary and oops

---
+++ reduce_fn.js
@@ -0,0 +1,4 @@
+let marks = [10, 20, 15, 46];
+
+let totalMarks = marks.reduce( (total, mark) => total += mark);
+console.log(totalMarks);
---
+++ ternary2.js
@@ -0,0 +1,5 @@
+let perc = 75;
+let grade = perc >= 90 ? 'A' :
+            perc >= 70 ? 'B' :
+            perc >= 50 ? 'C' : 'Fail';
+console.log(grade);
