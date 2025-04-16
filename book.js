43a4f6f790d631264bf7e45b44e571edc55fb7fd
mohammed-nuh <mohammednuhabuhuseina@gmail.com>
2025-04-16 19:55:50 +0530

    OOPS Assignments

--- book.js
+++ book.js
@@ -1,1 +1,18 @@
-this.
+class Book {
+    author;
+    title;
+    pages;
+
+    constructor(a, t, p){
+        this.author = a;
+        this.title = t;
+        this.pages = p;
+    }
+
+    printDetails(){
+        console.log(`Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}`);
+    }
+}
+
+let book1 = new Book("James Clear", "Atomic Habits", 200);
+book1.printDetails();
